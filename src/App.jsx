0b5a728b28e964df64281c5f42c001d879e8baff
Anurag Biswal <anugbswl@gmail.com>
2023-08-12 23:46:57 +0530

    deleted previous files

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-// home page
-import HomePageLayout from "./pages/homepage";
-import Landing from "./pages/Landing";
-
-// My Account
-import MyAccountLayout from "./components/myaccount/myAccountLayout";
-import MyAccountPage from "./components/myaccount/myAccount";
-import UserProfile from "./components/myaccount/profile";
-import UserAddress from "./components/myaccount/address";
-import UserPayment from "./components/myaccount/payment";
-import UserProductOrders from "./components/myaccount/orders";
-import AdminLayout from "./components/admin/adminLayout";
-import UserCart from "./components/myaccount/myCart";
-
-// product pages
-import Category from "./pages/Category";
-import Product from "./pages/product";
-
-
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <>
-            <Route path="/" element={<HomePageLayout/>}>
-              <Route index element={<Landing />} />
-              <Route path="category" element={<Category />} />
-              <Route path="product" element={<Product />} />
-            </Route>
-            <Route path="/myaccount" element={<MyAccountLayout />}>
-              <Route index element={<MyAccountPage />} />
-              <Route path="profile" element={<UserProfile />} />
-              <Route path="address" element={<UserAddress />} />
-              <Route path="payments" element={<UserPayment />} />
-              <Route path="orders" element={<UserProductOrders />} />
-              <Route path="cart" element={<UserCart />} />
-            </Route>
-            <Route path="/admin" element={<AdminLayout />} />
+            <Route path="/" element={<h1>Home</h1>} />
             <Route path="*" element={<h1>Not Found</h1>} />
           </>
         </Routes>
--- src/main.jsx
+++ src/main.jsx
@@ -2,28 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { NextUIProvider } from "@nextui-org/react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { register } from "swiper/element/bundle";
-import AOS from "aos";
-import "aos/dist/aos.css";
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-import { ContextProvider } from "./contextProvider/MyContext.jsx";
 
-// register Swiper custom elements
-register();
-// AOS init
-AOS.init();
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <NextUIProvider>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-    <ContextProvider>
-        <App />
-    </ContextProvider>
-      </LocalizationProvider>
-    </NextUIProvider>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <App />
+    </LocalizationProvider>
   </React.StrictMode>
 );
