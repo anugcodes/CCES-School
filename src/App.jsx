ea8374b69b75513ad3ccdfc83a12bd024f05dff6
Anurag Biswal <anugbswl@gmail.com>
2023-08-31 00:31:05 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,13 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import RootLayout from "./pages/root-layout";
-import HomePage from "./pages/home-page";
-import Aos from "aos";
-import 'aos/dist/aos.css';
-
-import { useEffect } from "react";
 
 function App() {
-
-  useEffect(()=> {
-    Aos.init();
-    Aos.refresh();
-  }, []);
-
   return (
     <>
       <BrowserRouter>
         <Routes>
           <>
-            <Route path="/" element={<RootLayout />}>
-              <Route index element={<HomePage />} />
-            </Route>
+            <Route path="/" element={<h1>Home</h1>} />
             <Route path="*" element={<h1>Not Found</h1>} />
           </>
         </Routes>
--- src/main.jsx
+++ src/main.jsx
@@ -3,30 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-import { LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-
-import {
-  ThemeProvider,
-  responsiveFontSizes,
-  createTheme,
-} from "@mui/material/styles";
-// responsive themes
-let theme = createTheme();
-theme = responsiveFontSizes(theme);
-
-import AOS from "aos";
-AOS.init({
-  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-});
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
-    </LocalizationProvider>
+    <App />
   </React.StrictMode>
 );
