1ac3c684350ad681d57238b9896c45e6c1191c4d
ankitBytes <sahuankitkumar99@gmail.com>
2023-09-20 23:30:37 +0530

    .

--- src/pages/survey.jsx
+++ src/pages/survey.jsx
@@ -117,23 +117,106 @@ export default function SurveyForm() {
                 section_form={SectionB7}
               />
 
-          {/* section B8: O and M */}
-          <SectionAccordion
-            expanded={expanded === "sectionB8"}
-            onChange={handleChange("sectionB8")}
-            heading="O and M"
-            section_form={SectionB8}
-          />
+              {/* section B8: O and M */}
+              <SectionAccordion
+                expanded={expanded === "sectionB8"}
+                onChange={handleChange("sectionB8")}
+                heading="O and M"
+                section_form={SectionB8}
+              />
 
-          {/* section B9: Capacity Building and Behaviour Change */}
-          <SectionAccordion
-            expanded={expanded === "sectionB9"}
-            onChange={handleChange("sectionB9")}
-            heading="Capacity Building and Behaviour Change"
-            // section_form={}
-          />
-        </Stack>
-        </CustomTabPanel>
+              {/* section B9: Capacity Building and Behaviour Change */}
+              <SectionAccordion
+                expanded={expanded === "sectionB9"}
+                onChange={handleChange("sectionB9")}
+                heading="Capacity Building and Behaviour Change"
+                section_form={SectionB9}
+              />
+            </Stack>
+          </CustomTabPanel>
+          <CustomTabPanel value={tab} index={1}>
+              <Stack direction="column" spacing={0}>
+                {/* section A - primary Informaiton */}
+                <SectionAccordion
+                  expanded={expanded === "sectionA"}
+                  onChange={handleChange("sectionA")}
+                  heading="Primary Informaiton"
+                  section_form={SectionA}
+                />
+
+                {/* section B1: Risk assessment, analysis, preventive measures, Plan */}
+                <SectionAccordion
+                  expanded={expanded === "sectionB1"}
+                  onChange={handleChange("sectionB1")}
+                  heading="Risk assessment, analysis, preventive measures, Plan"
+                  section_form={SectionB1}
+                />
+
+                {/* section B1: Water */}
+                <SectionAccordion
+                  expanded={expanded === "sectionB2"}
+                  onChange={handleChange("sectionB2")}
+                  heading="Water"
+                  section_form={SectionB2}
+                />
+
+                {/* section B3: Sanitation */}
+                <SectionAccordion
+                  expanded={expanded === "sectionB3"}
+                  onChange={handleChange("sectionB3")}
+                  heading="Sanitation"
+                  section_form={SectionB3}
+                />
+
+                {/* section B4: Handwashing with soap */}
+                <SectionAccordion
+                  expanded={expanded === "sectionB4"}
+                  onChange={handleChange("sectionB4")}
+                  heading="Handwashing with soap"
+                  section_form={SectionB4}
+                />
+
+                {/* section B5: Waste Management */}
+                <SectionAccordion
+                  expanded={expanded === "sectionB5"}
+                  onChange={handleChange("sectionB5")}
+                  heading="Waste Management"
+                  section_form={SectionB5}
+                />
+
+                {/* section B6: Energy */}
+                <SectionAccordion
+                  expanded={expanded === "sectionB6"}
+                  onChange={handleChange("sectionB6")}
+                  heading="Energy"
+                  section_form={SectionB6}
+                />
+
+                {/* section B7: Environment */}
+                <SectionAccordion
+                  expanded={expanded === "sectionB7"}
+                  onChange={handleChange("sectionB7")}
+                  heading="Environment"
+                  section_form={SectionB7}
+                />
+
+                {/* section B8: O and M */}
+                <SectionAccordion
+                  expanded={expanded === "sectionB8"}
+                  onChange={handleChange("sectionB8")}
+                  heading="O and M"
+                  section_form={SectionB8}
+                />
+
+                {/* section B9: Capacity Building and Behaviour Change */}
+                <SectionAccordion
+                  expanded={expanded === "sectionB9"}
+                  onChange={handleChange("sectionB9")}
+                  heading="Capacity Building and Behaviour Change"
+                  // section_form={}
+                />
+              </Stack>
+            </CustomTabPanel>
         </Box>
       </Container>
     </div>
