110db56db396b8eb217209ef3ff76b8e5885e720
ankitBytes <sahuankitkumar99@gmail.com>
2023-09-30 11:42:32 +0530

    changes in the dashboard

--- src/pages/admin/dashboard.jsx
+++ src/pages/admin/dashboard.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack"
 
 import { onSnapshot, collection } from "firebase/firestore";
 import { db } from "../../firebase";
@@ -10,6 +11,8 @@ import { db } from "../../firebase";
 import DashboardNav from "../../components/admin/dashboard-nav";
 import SchoolDataTab from "../../components/admin/school-data";
 import FormDataCharts from "../../components/admin/form-charts";
+
+import AdminImage from "../../../public/admin-dashboard.jpg"
 
 // context api
 import { serverData } from "../../contexts/formContexts";
@@ -65,15 +68,25 @@ export default function AdminDashboard() {
             margin: ".5rem 0",
             padding: "1rem",
             borderRadius: "1rem",
-            background: "#CEF6FF",
+            background: "#1cabe2",
           }}
         >
-          <Typography variant="subtitle1">
+          <Stack direction={"row"} spacing={4}>
+          <Stack spacing={3} justifyContent={"center"}>
+          <img src="https://www.unicef.org/india/sites/unicef.org.india/files/styles/logo/public/English.png?itok=7CkDc2wD" alt="" width={"80%"}/>
+          <Typography variant="h6" sx={{textTransform: "uppercase", fontWeight: "bold"}}>
             Total Registered Schools:{"  "}
-            <Typography variant="subtitle1" component={"span"}>
+            <Typography variant="h5" component={"span"} color={"white"} fontWeight={"bold"}>
               {primaryData.length}
             </Typography>
           </Typography>
+          </Stack>
+        <img src={AdminImage} alt="" width={"30%"}
+            style={{
+              borderRadius: "1rem"
+            }}
+           />
+        </Stack>
         </Box>
         <serverData.Provider value={{ primaryData, ccesData, sapData }}>
           {currentView === "schools" ? <SchoolDataTab /> : <FormDataCharts />}
