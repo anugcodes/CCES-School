798731413bccfc600b16f113d08914560f4fd5f4
rajkishorepatra <rpatrasm@gmail.com>
2023-10-09 21:22:34 +0530

    nav & footer update

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { FaFacebookF, FaTwitter, FaInstagram, FaYoutube, FaLinkedinIn } from 'react-icons/fa'
+import { AiOutlineMail } from 'react-icons/ai'
 
 function Footer() {
   return (
     <>
-      <div style={{ background: '#00aeef' }} className='p-3 text-white d-flex justify-content-around px-5 flex-wrap'>
+      <div style={{ background: '#00aeef' }} className='py-2 text-white d-flex justify-content-around px-5 flex-wrap'>
         <div>
-          <a href='https://www.unicef.org/about-unicef/contact-us' style={{ textDecoration: "none", color: "white" }}>Contact Us</a> | <a href='https://www.unicef.org/legal' style={{ textDecoration: "none", color: "white" }}>Legal</a>
+          {/* <a href='https://www.unicef.org/about-unicef/contact-us' style={{ textDecoration: "none", color: "white" }}>Contact Us</a> | <a href='https://www.unicef.org/legal' style={{ textDecoration: "none", color: "white" }}>Legal</a> */}
+          <p>For technical support,<br />
+            <AiOutlineMail color='white' size={20} />&nbsp;
+            mskhan2621957@gmail.com
+          </p>
         </div>
         <div className='d-flex flex-wrap'>
 
--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -5,32 +5,49 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 
-import logo from '../../img/logo.png'
+import logo1 from '../../img/logo1.png'
+import logo2 from '../../img/logo2.png'
+import logo3 from '../../img/logo3.jpg'
 
 
 export default function NavBar() {
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="sticky" style={{ background: '#00aeef' }} >
-                <Toolbar className='d-flex justify-content-between px-lg-5'>
-                    <img src={logo} alt='logo' width={200} height={100} />
-                    {
-                        (window.location.pathname.includes('/survey')) ?
-                            <>
-                                <Link to={{ pathname: "/" }}>
-                                    <Button variant="outlined" style={{ color: "white", borderWidth: "4px", borderColor: "white" }}>Home</Button>
-                                </Link>
-                            </>
-                            :
-                            <>
-                                <Link to={{ pathname: "/survey", hash: "sectionA" }}>
-                                    <Button variant="outlined" style={{ color: "white", borderWidth: "4px", borderColor: "white" }}>Take Survey</Button>
-                                </Link>
-                            </>
-                    }
+        <>
 
-                </Toolbar>
-            </AppBar>
-        </Box>
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar position="sticky" style={{ background: '#00aeef' }} >
+                    <Toolbar className='d-flex justify-content-between'>
+                        {/* <img src={logo1} alt='logo' width={200} height={100} /> */}
+                        <h2>CCES & WASH</h2>
+                        {
+                            (window.location.pathname.includes('/survey')) ?
+                                <>
+                                    <Link to={{ pathname: "/" }}>
+                                        <Button variant="outlined" style={{ color: "white", borderWidth: "4px", borderColor: "white" }}>Home</Button>
+                                    </Link>
+                                </>
+                                :
+                                <>
+                                    <Link to={{ pathname: "/survey", hash: "sectionA" }}>
+                                        <Button variant="outlined" style={{ color: "white", borderWidth: "4px", borderColor: "white" }}>Take Survey</Button>
+                                    </Link>
+                                </>
+                        }
+
+                    </Toolbar>
+                </AppBar>
+            </Box>
+
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar position="sticky" style={{ background: '#fff' }} >
+                    <Toolbar className='d-flex justify-content-around py-2 flex-wrap'>
+                        <img src={logo1} alt='logo' width={100} height={100} />
+                        <img src={logo3} alt='logo' width={100} height={100} />
+                        <img src={logo2} alt='logo' width={150} height={75} />
+
+                    </Toolbar>
+                </AppBar>
+            </Box>
+        </>
     );
 }
